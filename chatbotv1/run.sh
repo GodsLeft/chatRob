e676247842d337a1bc32af1dd86d163a6b1c810c
GodsLeft <1456466573@qq.com>
2017-03-07 10:31:29 +0800

    所有依赖打包

--- chatbotv1/run.sh
+++ chatbotv1/run.sh
@@ -1,1 +1,2 @@
-java -cp target/chatbotv1-1.0-SNAPSHOT.jar com.left.Indexer ../README.md ./zhuindex
+#java -cp target/chatbotv1-1.0-SNAPSHOT.jar com.left.Indexer ../README.md ./zhuindex
+java -cp target/chatbotv1-1.0-SNAPSHOT-jar-with-dependencies.jar com.left.Indexer ../corpus/srt.out ./zhuindex
