a717cf71adf50f328c9f3902fe3e43d48e6bca6c
GodsLeft <1456466573@qq.com>
2017-03-18 18:03:57 +0800

    small changes

--- wordvec/run.sh
+++ wordvec/run.sh
@@ -6,4 +6,10 @@
 
 # 加载向量
 # python word_vectors_loader.py vectors.bin
-python loadvec.py vectors.bin
+# python loadvec.py vectors.bin
+
+# 对甄嬛传进行分词
+# python ./word_segment.py zhenhuanzhuan.txt zhenhuanzhuan.segment
+
+# 生成单词向量
+./word2vec/word2vec -train ./zhenhuanzhuan.segment -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
